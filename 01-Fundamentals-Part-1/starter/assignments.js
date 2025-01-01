8fc7e53c91100dbf6c0b61c4c3dae375cd8f9d85
thesongmartins <songmartins01@gmail.com>
2025-01-01 21:49:48 +0100

    2025 LockedIn: Basic Operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -34,3 +34,31 @@ console.log(typeof isIsland, typeof population, typeof country, typeof language)
 */
 language = 'oegworok';
 console.log(language);
+
+// Basic Operators
+/*
+1. If your country split in half, and each half would contain half the population, then how many people would live in each half?
+
+2. Increase the population of your country by 1 and log the result to the console.
+
+3. Finland has a population of 6 million. Does your country have more people than Finland?
+
+4. The average population of a country is 33 million people. Does you country have less people than the average country?
+
+5. Based on the variables you created, create a new variable description which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'.
+ */
+let halfPopulation = population / 2;
+console.log(halfPopulation); // 100
+
+population++;
+// halfPopulation = population++;
+console.log(population); // 201 201
+
+const finlandPopulation = 6000000;
+console.log(population > finlandPopulation); //false
+
+const averagePopulation = 33000000;
+console.log(population < averagePopulation); // true
+
+const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+console.log(description);
