199bb4ede2f750eafc5f19dfc45d83c5500c8318
thesongmartins <songmartins01@gmail.com>
2025-01-01 17:06:41 +0100

    2025 LockedIn: Values N Variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,14 @@
+/*
+Values and Variables﻿
+1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+2. Log their values to the console.
+ */
+
+let country = "Nigeria";
+let continent = 'Africa';
+let population = 200;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+let js = 'amazing';
+// if (js === 'amazing') { alert("Javascript is Fun") };
+console.log(js);
+console.log(40 + 8 + 23 - 10);
+console.log('Jonas');
+console.log(23);
+
+// Values And Variables
+let firstName = 'Matilda';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+console.log(myFirstJob);
+
+// Data Types
+
