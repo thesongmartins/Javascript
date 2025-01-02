c446fcb4dc9ab720c0395571cff199a909347b81
thesongmartins <songmartins01@gmail.com>
2025-01-02 18:44:11 +0100

    2025 LockedIn: Equality Operators: == N ===

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -206,6 +206,32 @@ if (height) {
     console.log('Height is UNDEFINED');
 }
 
+// Equality Operators: == vs ===
+// == does type coercion 
+
+const agee = 18;
+if (agee === 18) console.log("You just became an adult (strict)");
+
+console.log(18 === 18); // true 
+console.log(18 === '18'); // false
+
+// const favorite = prompt("What's your favorite number?");
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) { // '23' == 23
+    console.log('Cool! 23 is an amazing number!');
+} else if (favorite === 7) {
+    console.log('7 is also a cool number');
+} else {
+    console.log('Number is not 23 or 7');
+}
+
+
+
+
+
+
 
 
 
