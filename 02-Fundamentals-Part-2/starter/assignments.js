6ecdc997c58c693a69bf2af6589b5ed40041fe3f
thesongmartins <songmartins01@gmail.com>
2025-01-03 16:03:43 +0100

    2025 LockedIn: Arrow Functions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -19,3 +19,37 @@ const russia = describeCountry("Russia", 143.8, 'Moscow');
 console.log(nigeria);
 console.log(usa);
 console.log(russia);
+
+// Functions Declarations and Expressions
+/*
+1. The world population is 7900 million people. Create a function declaration called percentageOfWorld1 which receives a population value, and returns the percentage of the world population that the given population represents. For example, China has 1441 million people, so it's about 18.2% of the world population.
+
+2. To calculate the percentage, divide the given population value by 7900 and then multiply by 100.
+
+3. Call percentageOfWorld1 for 3 populations of countries of your choice, store the results into variables, and log them to the console.
+
+4. Create a function expression which does the exact same thing, called percentageOfWolrd2, and also call it with 3 country populations (can be the same populations).
+
+
+ */
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100;
+};
+
+const percPortugal1 = percentageOfWorld1(10);
+const percChina1 = percentageOfWorld1(1441);
+const percUSA1 = percentageOfWorld1(332);
+
+console.log(percPortugal1, percChina1, percUSA1);
+
+// Arrow Functions
+const percentageOfWorld3 = population => (population / 7900) * 100;
+const percPortugal3 = percentageOfWorld3(10);
+const percChina3 = percentageOfWorld3(1441);
+const percUSA3 = percentageOfWorld3(332);
+
+console.log(percPortugal3, percChina3, percUSA3);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -30,3 +30,34 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
+// Function Declarations Vs Expressions
+function calAge1(birthYear) {
+    const age = 2037 - birthYear;
+    return age;
+} // Function declaration
+
+const age1 = calAge1(1991);
+// console.log(age1);
+
+const calAge2 = function (birthYear) {
+    return 2037 - 1991;
+} //Function expression
+
+const age2 = calAge2(1991);
+console.log(age1, age2);
+
+// Arrow Functions
+const calAge3 = birthYear => 2037 - birthYear;
+const age3 = calAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+
+
+
