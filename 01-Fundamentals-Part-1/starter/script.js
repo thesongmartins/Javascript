9a0ba8fcdaaac23ddc19088c500895744f2d1407
thesongmartins <songmartins01@gmail.com>
2025-01-03 12:06:16 +0100

    2025 LockedIn: Statements and Expressions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -297,6 +297,18 @@ if (day === 'Monday') {
 }
 
 // Statements and Expressions
+3 + 4
+1991
+true && false && !false
+
+if (23 > 10) {
+    const str = '23 is bigger';
+}
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+// The Conditional (Ternary) Operator
 
 
 
