61a17a32472223b4232c12a1cfb3b1251c74ff38
thesongmartins <songmartins01@gmail.com>
2025-01-04 04:02:23 +0100

    2025 LockedIn: Functions calling other functions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -47,9 +47,38 @@ const percUSA1 = percentageOfWorld1(332);
 console.log(percPortugal1, percChina1, percUSA1);
 
 // Arrow Functions
+/*
+ 1. Recreate the last assignment, but this time create an arrow function called percentageOfWorld3.
+*/
 const percentageOfWorld3 = population => (population / 7900) * 100;
 const percPortugal3 = percentageOfWorld3(10);
 const percChina3 = percentageOfWorld3(1441);
 const percUSA3 = percentageOfWorld3(332);
 
 console.log(percPortugal3, percChina3, percUSA3);
+
+// Functions calling other functions
+/*
+ 1. Create a function called describePopulation. Use the function type you like the most. This function takes in two arguments: country and population, and returns a strings like this: 'China has 1441 million people, which is about 18.2% of the world'.
+
+ 2. To calculate the percentage, describePopulation calls the percentageOfWorld1 you created earlier.
+
+ 3. Call describePopulation with data for 3 countries of your choice.
+*/
+
+const describePopulation = function (country, population) {
+    const percentage = percentageOfWorld1(population);
+    return `${country} has ${population} million people, which is about ${percentage} % of the world.`;
+}
+console.log(describePopulation('China', 1441));
+
+// const describePopulation = function (country, population) {
+//     const percentage = percentageOfWorld1(population);
+//     const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+//     console.log(description);
+// };
+
+// describePopulation('Portugal', 10);
+// describePopulation('China', 1441);
+// describePopulation('USA', 332);
+
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -59,5 +59,23 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 }
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 
+// Functions calling other functions
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    // console.log(apples, oranges);
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+
+
+
+
 
 
