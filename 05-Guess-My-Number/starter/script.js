2e5e5727b9dd79844a55ac9dfb39a6f5ada7b50b
thesongmartins <songmartins01@gmail.com>
2025-01-12 14:18:55 +0100

    2025 LockedIn: Implementing game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,8 +1,41 @@
 'use strict';
 
-const message = (document.querySelector('.message').textContent =
-  'Correct Number!');
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
-document.querySelector('.guess').value = 23;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    // When guess is too High
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    // document.querySelector('.message').textContent = 'Too low';
+    // score--;
+    // document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
