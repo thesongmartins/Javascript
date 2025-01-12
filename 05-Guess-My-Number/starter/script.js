39d78be875cd334f207275ba9daea48f9f0f6168
thesongmartins <songmartins01@gmail.com>
2025-01-12 02:25:15 +0100

    2025 LockedIn: Selecting and manipulating elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const message = (document.querySelector('.message').textContent =
+  'Correct Number!');
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+document.querySelector('.guess').value = 23;
