a8ec3b2e93f437533db4718e644f9077a633b1c0
thesongmartins <songmartins01@gmail.com>
2025-01-14 23:51:27 +0100

    2025 LockedIn: Working with classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,4 +2,21 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelector('.open-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log(btnOpenModal[i].addEventListener('click', openModal));
+}
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
