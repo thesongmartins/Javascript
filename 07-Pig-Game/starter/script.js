d5508705bcb44ab561c7c377b7956b544ffada85
thesongmartins <songmartins01@gmail.com>
2025-01-15 02:35:51 +0100

    2025 LockedIn: Resetting Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,16 +12,6 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
-
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
@@ -29,6 +19,29 @@ const switchPlayer = () => {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+let scores, currentScore, activePlayer, playing;
+
+// Starting conditions
+const initials = () => {
+  diceEl.classList.add('hidden');
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+initials();
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', () => {
@@ -76,4 +89,8 @@ btnHold.addEventListener('click', () => {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener('click', () => {
+  initials;
 });
