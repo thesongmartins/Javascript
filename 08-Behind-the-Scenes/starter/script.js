b49e1e3d7a3762a41fc3201b632228ca53a2aff0
thesongmartins <songmartins01@gmail.com>
2025-01-15 14:35:45 +0100

    2025 LockedIn: Scoping in practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -3,8 +3,21 @@
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   function printAge() {
-    const output = `You're ${age}, born in ${birthYear}`;
+    let output = `${firstName}, You're ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const firstName = 'Steven';
+      const str = `Oh and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'NEW OUTPUT';
+    }
+    // console.log(add(2, 3));
   }
   printAge();
   return age;
