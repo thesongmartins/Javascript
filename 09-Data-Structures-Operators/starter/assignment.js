1f8aee1c9ffee46eb1aa06edfd9d19b16aa560aa
thesongmartins <martinsjehoshaphat@gmail.com>
2025-03-06 04:25:57 +0100

    Logical assignment operators Assignments

--- 09-Data-Structures-Operators/starter/assignment.js
+++ 09-Data-Structures-Operators/starter/assignment.js
@@ -379,3 +379,16 @@ for (let i = 0; i < books.length; i++) {
   books[i].onlineContent ??
     console.log(`${books[i].title} provides no data about its online content`);
 }
+
+// Logical Assignment Operators
+// 7.1
+for (let i = 0; i < books.length; i++) {
+  books[i].edition ||= 1;
+  console.log(books[i].edition);
+}
+
+// 7.2
+for (let i = 0; i < books.length; i++) {
+  books[i].highlighted &&= !(books[i].thirdParty.goodreads.rating < 4.2);
+  console.log(books[i].highlighted);
+}
