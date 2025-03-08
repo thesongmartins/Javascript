25ad88d141b2cd75ccb98e1e35327942cc121b1a
thesongmartins <martinsjehoshaphat@gmail.com>
2025-03-08 23:13:28 +0100

    LockedIn 2025: Challenge 1

--- 09-Data-Structures-Operators/starter/assignment.js
+++ 09-Data-Structures-Operators/starter/assignment.js
@@ -392,3 +392,104 @@ for (let i = 0; i < books.length; i++) {
   books[i].highlighted &&= !(books[i].thirdParty.goodreads.rating < 4.2);
   console.log(books[i].highlighted);
 }
+
+// Coding Challenge #
+const football = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandoski', 'Gnarby', 'Lewandoski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1)
+const players1 = [
+  'Neuer',
+  'Pavard',
+  'Martinez',
+  'Alaba',
+  'Davies',
+  'Kimmich',
+  'Goretzka',
+  'Coman',
+  'Muller',
+  'Gnarby',
+  'Lewandowski',
+];
+
+const players2 = [
+  'Burki',
+  'Schulz',
+  'Hummels',
+  'Akanji',
+  'Hakimi',
+  'Weigl',
+  'Witsel',
+  'Hazard',
+  'Brandt',
+  'Sancho',
+  'Gotze',
+];
+// 2)
+const [neuer, ...fieldPlayers] = players1;
+console.log(neuer, fieldPlayers);
+
+// 3)
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4)
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5)
+const { team1, x: draw, team2 } = football.odds;
+console.log(team1, draw, team2);
+
+// 6)
+const printGoals = (...players) => {
+  players.forEach(player => {
+    console.log(player);
+  });
+  console.log(`Total goals: ${players.length}`);
+};
+printGoals(football.scored);
+
+// 7)
+const likelyWIn =
+  (football.odds.team1 > football.odds.team2 && football.odds.team1) ||
+  football.odds.team2;
+console.log(`${likelyWIn} is likely to win`);
