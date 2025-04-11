3760039d1a15ad647c1b1d24c06226a662cc081a
thesongmartins <martinsjehoshaphat@gmail.com>
2025-04-11 15:11:15 +0100

    LockedIn 2025: Creating Dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -346,3 +346,21 @@ console.log(new Date(2037, 10, 19, 15, 23, 5));
 
 console.log(new Date(0));
 console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with Dates
+const future = new Date(2037, 10, 19, 15, 23, 5);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString);
+console.log(future.getTime());
+
+console.log(new Date());
+console.log(Date.now());
+
+future.setFullYear(2040);
