d85f55f263168c6e6a2e1d46b6fd9020aad835d1
thesongmartins <martinsjehoshaphat@gmail.com>
2025-04-14 22:45:40 +0100

    LockedIn 2025: Types of events and event handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -36,6 +36,22 @@ btnScrollTo.addEventListener('click', e => {
   // });
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+// Event Propagation: Bubbling and Capturing
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)},${randomColor(0, 255)})`;
+// console.log(randomColor(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor(0, 255);
+});
+document.querySelector('.nav__links').addEventListener('click', e => {
+  console.log('LINK');
+});
+document.querySelector('.nav').addEventListener('click', e => {
+  console.log('LINK');
+});
 
 const openModal = function (e) {
   e.preventDefault();
