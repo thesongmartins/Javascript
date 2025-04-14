6857c68e3765720e6ca6e134b37d35e069c6f028
thesongmartins <martinsjehoshaphat@gmail.com>
2025-04-14 21:40:32 +0100

    LockedIn 2025: Types of events and event handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -133,9 +133,14 @@ console.log(link.getAttribute('href'));
 
 // Types of events and event handlers
 const h1 = document.querySelector('h1');
-h1.addEventListener('mouseenter', e => {
+const alertH1 = e => {
   alert('addEventListener: Great! You are reading the heading');
-});
+};
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => {
+  h1.removeEventListener('mouseenter', alertH1);
+}, 3000);
 h1.onmouseenter = e => {
   alert('addEventListener: Great! You are reading the heading');
 };
