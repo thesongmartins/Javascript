f7498e50a990b91ecc6f0905476ad29fb26265bb
thesongmartins <martinsjehoshaphat@gmail.com>
2025-04-15 12:10:37 +0100

    LockedIn 2025: Dom Traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -195,3 +195,16 @@ message.style.height =
 // });
 
 // Dom Traversing
+const h1 = document.querySelector('h1');
+
+// Going dwonwards: Child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+
+// Going Upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
