9649c5a7ad2bb597140b51bb6daccd6eae097370
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-10 20:50:42 +0100

    LockedIn 2025: OOP Setters & Getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -98,8 +98,8 @@ bmw.brake();
 
 // Class Declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -107,9 +107,22 @@ class PersonCl {
   calcAge() {
     console.log(2037 - this.birthYear);
   }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  // Set a property that already exist
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not s full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davies', 1996);
 console.log(jessica);
 jessica.calcAge();
 
@@ -123,3 +136,20 @@ jessica.greet();
 // 1. Classes are NOT hoisted
 // 2. Classes are first class citizens
 // 3. Classes are executed in strict mode
+
+// Setters & Getters
+const walter = new PersonCl('Walter White', 1965);
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
