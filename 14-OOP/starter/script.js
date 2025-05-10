62486b1ac060777756568fe17b25ed243b81c058
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-10 04:00:09 +0100

    LockedIn 2025: OOP Constructor functions & new operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+const Person = function (firstName, birthYear) {};
+
+new Person('Jonas', 1999);
+
+// 1. New Empty is created
+// 2. function is called, this = {}
+// 3. {} is linked to a prototype
+// 4. function automatically returns {}
