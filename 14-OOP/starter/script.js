0ec7709d551f6dafb92330e3eccf65ebd2124b6e
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-10 19:40:34 +0100

    LockedIn 2025: OOP Challenge 1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -70,3 +70,24 @@ console.log(arr.unique());
 
 // const h1 = document.querySelector('h1');
 // console.dir(x => x + 1);
+
+// Challenge
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Benz', 95);
+
+bmw.accelerate();
+bmw.brake();
