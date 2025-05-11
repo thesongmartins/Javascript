f8c0a4b545971541acbd3af0601737db67b2ad73
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-11 03:36:21 +0100

    LockedIn 2025: OOP Challenge 2

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -187,3 +187,35 @@ console.log(steven.__proto__ === PersonProto);
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1979);
 sarah.calcAge();
+
+// Challenge
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed = this.speed + 10;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+  }
+
+  brake() {
+    this.speed = this.speed - 10;
+    console.log(`${this.make} is going at ${this.speed}km/h`);
+  }
+
+  get speedUs() {
+    return this.speed / 1.6;
+  }
+
+  set speedUs(speed) {
+    return (this.speed = speed * 1.6);
+  }
+}
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUs);
+ford.accelerate();
+ford.brake();
+ford.speedUs = 50;
+console.log(ford);
