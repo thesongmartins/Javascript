abcf2dd58b0e1c149810ff2a0f4e6566b3970526
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-12 01:16:39 +0100

    LockedIn 2025: Challenge 3

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -241,3 +241,22 @@ mike.calcAge();
 
 console.log(mike.__proto__);
 console.log(mike.__proto__.__proto__);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
+
+// Challenge 3
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// Linking Prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
