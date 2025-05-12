a1ec6e7936ced030748fa73f113d7cc5b62dddc0
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-12 01:20:57 +0100

    LockedIn 2025: Challenge 3

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -258,5 +258,12 @@ EV.prototype.chargeBattery = function (chargeTo) {
   this.charge = chargeTo;
 };
 
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
 const tesla = new EV('Tesla', 120, 23);
 tesla.chargeBattery(90);
