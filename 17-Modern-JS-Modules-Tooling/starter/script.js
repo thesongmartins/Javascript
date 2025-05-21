7272b61e4c6f34f9949d203508f8baa2f577e2c4
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-21 07:33:32 +0100

    LockedIn 2025: Top Level Await ES(2022)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -2,10 +2,29 @@
 // import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
 
 // import * as ShoppingCart from './shoppingCart.js';
-console.log('Importing Module');
+// console.log('Importing Module');
 
 // addToCart('bread', 5);
 // console.log(totalPrice, totalQuantity);
 
-import add from './shoppingCart.js';
-add('pizza', 2);
+// import add from './shoppingCart.js';
+// add('pizza', 2);
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(post => console.log(post));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,6 +1,11 @@
 // Exporting and Importing in ES6 Modules
 console.log('Exporting Module');
 
+// Blocking code
+// console.log('Starting fetching users...');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finished fetching users');
+
 const shoppingCost = 10;
 const cart = [];
 
@@ -14,7 +19,7 @@ const totalQuantity = 23;
 
 export { totalPrice, totalQuantity };
 
-export default function (prooduct, quantity) {
-  cart.push({ prooduct, quantity });
-  console.log(`${quantity} ${prooduct} added to cart`);
-}
+// export default function (prooduct, quantity) {
+//   cart.push({ prooduct, quantity });
+//   console.log(`${quantity} ${prooduct} added to cart`);
+// }
