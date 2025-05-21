1c0a7389d2fdfc29c99658d51869aee0f4f01104
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-21 07:50:11 +0100

    LockedIn 2025: CommonJS

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -58,3 +58,13 @@ ShoppingCart2.addToCart('apple', 4);
 ShoppingCart2.addToCart('pizza', 2);
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
+
+// CommonJS Modules
+export.addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+    );
+  };
+
+import { addToCart } = require('./shoppingCart.js') ;
