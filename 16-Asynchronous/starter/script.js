4c298ac5e7ceb9c6e32ce0928286aca7ddbc94b4
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-21 05:38:31 +0100

    LockedIn 2025: Running Promises in Parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -447,3 +447,19 @@ const whereAmIi = async function (country) {
   }
   console.log('Finished getting location');
 })();
+
+// Running Promises in Parallel
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    const [data] = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]);
+    console.log(data);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.log(err);
+  }
+};
+get3Countries('portugal', 'canada', 'tanzania');
