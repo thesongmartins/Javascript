adabff03002ea6640a51bb47b44e67def4a7eb5d
thesongmartins <martinsjehoshaphat@gmail.com>
2025-05-28 03:37:27 -0700

    LockedIn 2025: Listening for load & hash events

--- 18-forkify/starter/dist/starter.4a59a05f.js
+++ 18-forkify/starter/dist/starter.4a59a05f.js
@@ -668,7 +668,8 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"7dWZ8":[function(require,module,exports,__globalThis) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-var _webImmediateJs = require("core-js/modules/web.immediate.js");
+var _webImmediateJs = require("core-js/modules/web.immediate.js"); // window.addEventListener('hashchange', showRecipe);
+ // window.addEventListener('load', showRecipe);
 var _iconsSvg = require("url:../img/icons.svg");
 var _iconsSvgDefault = parcelHelpers.interopDefault(_iconsSvg);
 var _runtime = require("regenerator-runtime/runtime");
@@ -694,10 +695,13 @@ const renderSpinner = (parentEl)=>{
     parentEl.insertAdjacentHTML('afterbegin', markup);
 };
 const showRecipe = async function() {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+    if (!id) return;
     try {
         // 1) Loading Recipe
         renderSpinner(recipeContainer);
-        const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
         const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} (${res.status}) `);
         console.log(res, data);
@@ -807,7 +811,11 @@ const showRecipe = async function() {
         console.log(err);
     }
 };
-showRecipe();
+// showRecipe();
+[
+    'hashchange',
+    'load'
+].forEach((e)=>window.addEventListener(e, showRecipe));
 
 },{"url:../img/icons.svg":"fd0vu","@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT","core-js/modules/web.immediate.js":"bzsBv","regenerator-runtime/runtime":"f6ot0"}],"fd0vu":[function(require,module,exports,__globalThis) {
 module.exports = module.bundle.resolve("icons.0809ef97.svg") + "?" + Date.now();
--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -29,12 +29,17 @@ const renderSpinner = parentEl => {
 };
 
 const showRecipe = async function () {
+  const id = window.location.hash.slice(1);
+  console.log(id);
+
+  if (!id) return;
+
   try {
     // 1) Loading Recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -161,4 +166,8 @@ const showRecipe = async function () {
     console.log(err);
   }
 };
-showRecipe();
+// showRecipe();
+
+['hashchange', 'load'].forEach(e => window.addEventListener(e, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
